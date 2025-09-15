eb5cc9f9aa8af5a7f04193aee015d0a67b8d9ce3
erik-per-lidstrom <kirelidstrom@gmail.com>
2025-09-15 14:47:38 +0200

    created a starter template for js

---
+++ JS/01-JS_intro/script.js
@@ -0,0 +1,32 @@
+const xy = "i dont change";
+let xz = "i change";
+xz = "i changed";
+console.log(xy);
+console.log(xz);
+console.log("Hello World!");
+
+let my_name = "Erik Lidström";
+console.log(typeof my_name, my_name);
+
+let age = 18;
+console.log(typeof age, age);
+
+let is_student = true;
+console.log(typeof is_student, is_student);
+
+age = age + 1;
+console.log(age);
+
+is_student = false;
+
+console.log(is_student);
+
+let introduction =
+  "i am " +
+  age +
+  " years old my name is " +
+  my_name +
+  " and my student status is " +
+  is_student;
+
+console.log(introduction);
